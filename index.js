0a666d654ef219320b98846c8a758e461f05cee2
max anselmo <itmanselmo@gmail.com>
2021-09-21 20:13:05 -0400

    completed assignment, creating functions using the array.filter function according to the test suite

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+function findMatching(collection, name) {
+    let returnList = [];
+    for(const driver of collection) {
+        if(driver.toLowerCase() == name.toLowerCase()) {
+            // console.log(name);
+            returnList.push(driver);
+        }
+    }
+    return returnList;
+}
+
+function fuzzyMatch(names, string) {
+    let returnList = [];
+    for(const driver of names) {
+        if(driver.substring(0, string.length) == string){
+            returnList.push(driver);
+        }
+    }
+    return returnList;
+}
+
+function matchName(drivers, string) {
+    let returnList = [];
+    for(const driver of drivers) {
+        if(driver.name == string) {
+            returnList.push(driver);
+        }
+    }
+    return returnList;
+}
